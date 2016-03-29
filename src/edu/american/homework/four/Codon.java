7ca3ac4fde5613e828f2f449075cea2dd1d35873
Adam Knapp <adam.knapp@instanton.org>
2016-03-29 18:35:21 -0400

    update

--- src/edu/american/homework/four/Codon.java
+++ src/edu/american/homework/four/Codon.java
@@ -7,14 +7,6 @@ package edu.american.homework.four;
 public class Codon {
     private final char[] codon;
 
-    public static void main(String[] args) {
-        char[] letters = {'A', 'C', 'G', 'T'};
-        for (char s1 : letters) for (char s2 : letters) for (char s3 : letters) {
-            Codon codon = new Codon(s1,s2,s3);
-            System.out.println(codon+" hc="+codon.hashCode());
-        }
-    }
-
     public Codon(String string) {
         if (string.length() != 3) throw new IllegalArgumentException();
         codon = new char[3];
@@ -36,7 +28,6 @@ public class Codon {
         }
     }
 
-
     public Codon(char one, char two, char three) {
         codon = new char[3];
         one = Character.toUpperCase(one);
@@ -52,14 +43,29 @@ public class Codon {
         codon[2] = three;
     }
 
-    public boolean equals(Codon other) {
-        return this.codon[0] == other.codon[0] && this.codon[1] == other.codon[1]
-                && this.codon[2] == other.codon[2];
+    public static void main(String[] args) {
+        char[] letters = {'A', 'C', 'G', 'T'};
+        for (char s1 : letters)
+            for (char s2 : letters)
+                for (char s3 : letters) {
+                    Codon codon = new Codon(s1, s2, s3);
+                    System.out.println(codon + " hc=" + codon.hashCode());
+                }
     }
 
     public int hashCode() {
         return 0;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Codon && equals((Codon) obj);
+    }
+
     public String toString() { return new String(codon); }
+
+    public boolean equals(Codon other) {
+        return this.codon[0] == other.codon[0] && this.codon[1] == other.codon[1]
+                && this.codon[2] == other.codon[2];
+    }
 }

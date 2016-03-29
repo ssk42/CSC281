4f6b8e6ecf02f7bdc34f7d2e5b072c8d75fbbcf0
Adam Knapp <adam.knapp@instanton.org>
2016-03-29 18:32:56 -0400

    threads

---
+++ src/edu/american/ThreadingExample.java
@@ -0,0 +1,36 @@
+package edu.american;
+
+/**
+ * Created by knappa on 3/28/16.
+ */
+public class ThreadingExample {
+
+    public static void main(String[] args) {
+
+        Thread threadOne = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.print("a");
+                }
+            }
+        });
+
+        Thread threadTwo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++)
+                    System.out.print("b");
+            }
+        });
+
+        threadOne.start();
+        threadTwo.start();
+    }
+
+}
